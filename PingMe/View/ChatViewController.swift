07beb2d6e441844423e44db1469af0c510111d5a
Abhishek <abhishek.marriala@gmail.com>
2020-05-06 23:03:00 +0530

    view

---
+++ PingMe/View/ChatViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ChatViewController.swift
+//  PingMe
+//
+//  Created by Abhishek Marriala on 28/04/20.
+//  Copyright © 2020 Abhishek Marriala. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+
+}
