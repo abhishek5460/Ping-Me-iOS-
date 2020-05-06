acdb9e446320f8cfda7d84b463e9d84fad6eed85
Abhishek <abhishek.marriala@gmail.com>
2020-05-06 23:03:22 +0530

    View

---
+++ PingMe/View/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  PingMe
+//
+//  Created by Abhishek Marriala on 28/04/20.
+//  Copyright © 2020 Abhishek Marriala. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    
+}
